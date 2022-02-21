f4a524f2b872e123d84f2ed604091a1b148f14da
SHILJITH MP <shiljith07@gmail.com>
2022-02-21 10:47:26 +0530

    graph implemented

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -78,59 +78,55 @@ router.get("/balance-work-value", auth, (req, res) => {
 });
 
 router.get("/month-wise-work-value", (req, res) => {
-  return res.json({
-    success: true,
-    data: {
-      labels: [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "Augest",
-        "September",
-        "October",
-        "November",
-        "December",
-      ],
-      datasets: [
-        {
-          data: [10, 15, 23, 12, 66, 43, 78, 10, 44, 33, 45, 15],
-          label: "Work Value",
-        },
-      ],
-    },
-  });
-  db.all(
-    `
-      SELECT COUNT(unitId) as totalWorkValue FROM project_units as pu 
-    `,
-    (error, result, fields) => {
-      if (error) {
-        return res
-          .status(404)
-          .json({ success: false, data: null, error: error });
-      }
-      return res.json({ success: true, data: result });
-    }
-  );
-});
-
-router.get("/project-status", auth, (req, res) => {
-  const projectStatus = {
-    labels: ["In Progress", "On Hold", "Completed"],
+  const monthWiseWorkValue = {
+    labels: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "Augest",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
     datasets: [
       {
-        data: [3, 2],
+        data: [],
+        label: "Work Value",
       },
     ],
   };
-
   const query = `
-    SELECT COUNT(*) as inProgress from projects
-    WHERE accountId = ${req.payload.accountId} AND status = 3  
+    select coalesce(sum(jan_est),0) as jan_est, coalesce(sum(feb_est),0) as feb_est, coalesce(sum(mar_est),0) as mar_est, coalesce(sum(apr_est),0) as apr_est, coalesce(sum(may_est),0) as may_est,
+    coalesce(sum(jun_est),0) as jun_est,coalesce(sum(jul_est),0) as jul_est,coalesce(sum(aug_est),0) as aug_est,coalesce(sum(sep_est),0) as sep_est,coalesce(sum(oct_est),0) as oct_est,
+    coalesce(sum(nov_est),0) as nov_est,coalesce(sum(dec_est),0) as dec_est from(
+    select 
+    case when month='01' then total_est else 0 end as jan_est,
+    case when month='02' then total_est else 0 end as feb_est,
+    case when month='03' then total_est else 0 end as mar_est,
+    case when month='04' then total_est else 0 end as apr_est,
+    case when month='05' then total_est else 0 end as may_est,
+    case when month='06' then total_est else 0 end as jun_est,
+    case when month='07' then total_est else 0 end as jul_est,
+    case when month='08' then total_est else 0 end as aug_est,
+    case when month='09' then total_est else 0 end as sep_est,
+    case when month='10' then total_est else 0 end as oct_est,
+    case when month='11' then total_est else 0 end as nov_est,
+    case when month='12' then total_est else 0 end as dec_est
+    from
+    (
+    select 
+    strftime('%m',startDate) as month,
+    sum(estimatedAmount) as total_est
+    from project_units
+    where strftime('%Y',startDate) = '2022'
+    group by strftime('%m',startDate)
+    )A
+    )B 
   `;
   db.get(query, (error, result, fields) => {
     if (error) {
@@ -138,7 +134,44 @@ router.get("/project-status", auth, (req, res) => {
     }
     if (result) {
       console.log("PSTATUS", result);
-      projectStatus.datasets[0].data.push(result.inProgress);
+      monthWiseWorkValue.datasets[0].data = Object.values(result);
+      return res.json({ success: true, data: monthWiseWorkValue });
+    } else {
+      return res.status(404).send({ message: "Not found" });
+    }
+  });
+});
+
+router.get("/project-status", auth, (req, res) => {
+  const projectStatus = {
+    labels: ["In Progress", "On Hold", "Completed"],
+    datasets: [
+      {
+        data: [],
+      },
+    ],
+  };
+
+  const query = `
+  select sum(InProgress_count) as InProgress_count, sum(hold_count) as hold_count, sum(completed_count) as completed_count from(
+    select 
+    case when status = 1 then sumone
+      else 0 end as InProgress_count,
+    case when status = 2 then sumone
+      else 0 end as hold_count,
+    case when status = 3 then sumone
+      else 0 end as completed_count from (
+      SELECT status, count(*) as sumOne  from projects WHERE accountId = ${req.payload.accountId} GROUP by status 
+    )A
+  )B 
+  `;
+  db.get(query, (error, result, fields) => {
+    if (error) {
+      return res.status(404).json({ success: false, data: null, error: error });
+    }
+    if (result) {
+      console.log("PSTATUS", result);
+      projectStatus.datasets[0].data = Object.values(result);
       return res.json({ success: true, data: projectStatus });
     } else {
       return res.status(404).send({ message: "Not found" });
